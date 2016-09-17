c6688c0d16e7e66f4c43640eb78efe0ba33c9690
Bob <bobthekingofegypt@googlemail.com>
2016-09-17 15:11:40 +0100

    initial commit

---
+++ src/test/java/org/bobstuff/swarmtest/HelloWorldEndpointTest.java
@@ -0,0 +1,12 @@
+package org.bobstuff.swarmtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldEndpointTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+	}
+}
